125d6909e852d2485fada70224625dbb9fc5adb8
Miguel Guerrero <miguel.a.guerrero@gmail.com>
2019-04-15 16:35:01 -0700

    improvements to sv_parse.sh

--- java/Testjson.java
+++ java/Testjson.java
@@ -6,7 +6,7 @@ import org.antlr.v4.runtime.CommonTokenStream;
 import org.antlr.v4.runtime.tree.ParseTree;
 import org.antlr.v4.runtime.tree.ParseTreeWalker;
 
-public class TestLisp {
+public class Testjson {
 
     public static void main(String[] args) throws IOException {
         
@@ -15,14 +15,14 @@ public class TestLisp {
             fileName = args[0];
 
         ANTLRInputStream input = new ANTLRFileStream(fileName);
-        
+
         SvLexer lexer = new SvLexer(input);
         CommonTokenStream tokens = new CommonTokenStream(lexer);
         
         SvParser parser = new SvParser(tokens);
         ParseTree tree = parser.source_text();
 
-        String dump = TreeUtils.toLispStringTree(tree, parser);
+        String dump = TreeUtils.toJsonStringTree(tree, parser);
 
         System.out.println(dump);
     }
--- sv_parse.sh
+++ sv_parse.sh
@@ -1,21 +1,33 @@
 #!/bin/bash
 
 if [ "$1" == "" ]; then
-    echo "Usage: $0 filename ..."
+    echo "Usage: $0 [-python] [-lisp] filename ..."
     exit 1
 fi
 
 dir=`dirname $0`
 export CLASSPATH="${dir}/java:/usr/local/lib/antlr-4.7.1-complete.jar:${CLASSPATH}" 
 
+pyth=0
+ext=json
 while [ "$1" != "" ]; do
-    if [ -f "$1" ]; then
+    if [ "$1" == "-python" ]; then
+        pyth=1
+    elif [ "$1" == "-lisp" ]; then
+        ext=lisp
+    elif [ -f "$1" ]; then
         inpBase=`basename $1`
 
+        echo "=== preprocessing $1 into $inpBase.post" 
         vppreproc $1 --simple > $inpBase.post
         if [ $? == 0 ]; then
-            echo "=== generating $inpBase.json" 
-            java TestJson $inpBase.post > $inpBase.json 
+            if [ $pyth == 0 ]; then
+                echo "=== generating $inpBase.$ext with java" 
+                java Test$ext $inpBase.post > $inpBase.$ext 
+            else
+                echo "=== generating $inpBase.$ext with python" 
+                ${dir}/python/TestSvVisitor.py $inpBase.post $inpBase.$ext
+            fi
         else
             echo "# Error pre-processing $1, skipping it"
         fi
