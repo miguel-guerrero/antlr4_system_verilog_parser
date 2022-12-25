52362486117d5174a7fde6dc065a14d3f42655de
Miguel Guerrero <ma_gm@yahoo.com>
2022-12-24 18:30:10 -0800

    cleanup sv_parse.sh. bug fix on regress.sh after TESTS->CORPUS rename.

--- bin/sv_parse.sh
+++ bin/sv_parse.sh
@@ -1,21 +1,27 @@
 #!/bin/bash
 #set -o xtrace
 
-if [ "$1" == "" ]; then
-    echo "Usage: $0 [-python|-java] [-lisp|-json|-xml] [-d outdir] filename ..."
+help() {
     echo " "
-    echo " -d outdir : specify output directory"
+    echo "Usage: $0 [-python|-java] [-lisp|-json|-xml] [-d outdir] filename ..."
     echo " "
     echo "Code binding to use is defined by the following:"
     echo "  -python  : to use python binding"
-    echo "  -java    : to use java binding - default"
+    echo "  -java    : to use java binding (default)"
     echo " "
-    echo "Output format is defined by the following"
-    echo "  -lisp    : to geerate lisp style output"
-    echo "  -json    : to geerate JSON output"
-    echo "  -xml     : to geerate XML output - default"
+    echo "Output format is defined by the following:"
+    echo "  -lisp    : to generate lisp style output"
+    echo "  -json    : to generate JSON output"
+    echo "  -xml     : to generate XML output (default)"
+    echo " "
+    echo " -d outdir : specify output directory"
+    echo " -h        : provide this help"
     echo " "
     exit 1
+}
+
+if [ "$1" == "" ]; then
+    help
 fi
 
 
@@ -30,21 +36,17 @@ src_path="$(dirname $src_abs)"
 # define vars for top directory
 top_bin=$src_path
 top="$top_bin/.."
-JAR=$(ls $top_bin/antlr*.jar)
-export CLASSPATH="${top}/java:$JAR:$CLASSPATH"
-echo "CLASSPATH=$CLASSPATH"
 
-# choose pypy3 if available
-pyth=$(which pypy3 2> /dev/null)
-if [ ! -x $pyth ]; then
-    pyth=python3
-fi
 
+# set option defaults
 pyth_binding=0
 ext=xml
 outDir="./"
+inpFiles=()
 while [ "$1" != "" ]; do
-    if [ "$1" == "-python" ]; then
+    if [ "$1" == "-h" ]; then
+        help
+    elif [ "$1" == "-python" ]; then
         pyth_binding=1
     elif [ "$1" == "-java" ]; then
         pyth_binding=0
@@ -61,38 +63,52 @@ while [ "$1" != "" ]; do
             mkdir $outDir
         fi
     elif [ -f "$1" ]; then
-        inpBase=`basename $1`
-        postProc="$outDir/post.$inpBase"
-        $top_bin/sv_preproc.sh $1 $postProc
-        if [ $? != 0 ]; then
-            echo "Some error occurred preprocessing $1"
-            exit 1
-        fi
-        fileOut="$outDir/$inpBase.$ext"
-        if [ $pyth_binding == 0 ]; then
-            echo "=== generating $inpBase.$ext (using java binding) ===" 
-            if [ "$ext" == "xml" ]; then
-                # xml is generated doing first JSON and then converting to XML
-                java Testjson $postProc | $top_bin/json2xml_filter.py > $fileOut
-            else
-                java Test$ext $postProc > $fileOut
-            fi
-        else
-            echo "=== generating $inpBase.$ext (using python binding) ===" 
-            if [ "$ext" == "xml" ]; then
-                # xml is generated doing first JSON and then converting to XML
-                ${top}/python/TestSvVisitor.py $postProc /dev/stdout | $top_bin/json2xml_filter.py > $fileOut
-            else
-                $pyth ${top}/python/TestSvVisitor.py $postProc $fileOut
-            fi
-        fi
-        if [ $? != 0 ]; then
-            echo "Some error occurred during parsing $1"
-            exit 1
-        fi
-        rm -f $postProc
+        inpFiles+=("$1")
     else
         echo "# Warning: skipping $1, not found/not an option"
     fi
     shift
+done
+
+if [ $pyth_binding == 0 ]; then
+    # setup paths for java binding
+    JAR=$(ls $top_bin/antlr*.jar)
+    export CLASSPATH="${top}/java:$JAR:$CLASSPATH"
+    echo "CLASSPATH=$CLASSPATH"
+else
+    # choose pypy3 if available
+    pyth=$(which pypy3 2> /dev/null)
+    if [ ! -x $pyth ]; then
+        pyth=python3
+    fi
+    echo "python=$pyth"
+fi
+
+# process all files
+for inpFile in "${inpFiles[@]}"; do
+    inpBase=`basename $inpFile`
+    postProc="$outDir/post.$inpBase"
+    $top_bin/sv_preproc.sh $inpFile $postProc
+    if [ $? != 0 ]; then
+        echo "Some error occurred preprocessing $inpFile"
+        exit 1
+    fi
+    fileOut="$outDir/$inpBase.$ext"
+    if [ $pyth_binding == 0 ]; then
+        echo "=== generating $inpBase.$ext (using java binding) ===" 
+        if [ "$ext" == "xml" ]; then
+            # xml is generated doing first JSON and then converting to XML
+            java Testjson $postProc | $top_bin/json2xml_filter.py > $fileOut
+        else
+            java Test$ext $postProc > $fileOut
+        fi
+    else
+        echo "=== generating $inpBase.$ext (using python binding) ===" 
+        $pyth ${top}/python/TestSvVisitor.py $postProc $fileOut
+    fi
+    if [ $? != 0 ]; then
+        echo "Some error occurred during parsing $inpFile"
+        exit 1
+    fi
+    rm -f $postProc
 done
--- regress.sh
+++ regress.sh
@@ -2,10 +2,10 @@
 
 echo "Running regression with java binding"
 rm -rf  out_java 2> /dev/null
-time ./sv_parse.sh -d out_java -java -lisp ./TESTS/*.sv 
+time ./sv_parse.sh -d out_java -java -lisp ./CORPUS/*.sv 
 
 echo "Running regression with python binding"
 rm -rf  out_python 2> /dev/null
-time ./sv_parse.sh -d out_python -python -lisp ./TESTS/*.sv 
+time ./sv_parse.sh -d out_python -python -lisp ./CORPUS/*.sv 
 
 diff -r out_java out_python
